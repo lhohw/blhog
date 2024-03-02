e3dcb3c8dda1c0607cec69ea96d00abacf86330e
lhohw <lho.hw.an1@gmail.com>
2024-03-02 16:37:03 +0900

    feat: set initial config

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Nunito_Sans } from "next/font/google";
+
+export const nunito_sans = Nunito_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        sea: {
+          100: "#ecfdf5",
+          200: "#d1fae5",
+          300: "#99f6e4",
+          400: "#5eead4",
+          500: "#22d3ee",
+          600: "#06b6d4",
+          700: "#0284c7",
+          800: "#0369a1",
+          900: "#1e40af",
+          1000: "#1e3a8a",
+          1100: "#172554",
+        },
+      },
+      borderWidth: {
+        slight: "0.5px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
