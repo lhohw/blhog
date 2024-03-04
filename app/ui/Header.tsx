0647b2bd2394ec4c28a7517e37ce5cb0fbb39842
lhohw <lho.hw.an1@gmail.com>
2024-03-04 18:01:04 +0900

    feat: Header component

---
+++ app/ui/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex flex-none h-10 w-full items-center pl-4">
+      <span className="text-base font-bold">{`lhohw's blog`}</span>
+    </header>
+  );
+}
