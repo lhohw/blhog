acf64056301799a160631fb62b7470b81f2828bb
lhohw <lho.hw.an1@gmail.com>
2024-03-05 10:23:13 +0900

    feat: change dynamic classname to static for tailwind

--- app/ui/icons.tsx
+++ app/ui/icons.tsx
@@ -1,4 +1,3 @@
-import { PropsWithChildren } from "react";
 import clsx from "clsx";
 
 export type IconProps = {
@@ -7,8 +6,13 @@ export type IconProps = {
   className?: string;
 };
 
-export const Menu = ({ strokeWidth = 1.5, ...props }: IconProps) => (
-  <IconWrapper {...props}>
+export const Menu = ({
+  size = 16,
+  className,
+  strokeWidth = 1.5,
+  ...props
+}: IconProps) => (
+  <div className={clsx(`w-4 h-4`, className)} {...props}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -23,11 +27,16 @@ export const Menu = ({ strokeWidth = 1.5, ...props }: IconProps) => (
         d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
       />
     </svg>
-  </IconWrapper>
+  </div>
 );
 
-export const ChevronLeft = ({ strokeWidth = 2, ...props }: IconProps) => (
-  <IconWrapper {...props} size={12}>
+export const ChevronLeft = ({
+  size = 12,
+  className,
+  strokeWidth = 2.5,
+  ...props
+}: IconProps) => (
+  <div className={clsx(`w-3 h-3`, className)} {...props}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -42,25 +51,19 @@ export const ChevronLeft = ({ strokeWidth = 2, ...props }: IconProps) => (
         d="m8.25 4.5 7.5 7.5-7.5 7.5"
       />
     </svg>
-  </IconWrapper>
+  </div>
 );
 
-export const JS = ({ strokeWidth, ...props }: IconProps) => (
-  <IconWrapper {...props}>
+export const JS = ({
+  size = 16,
+  className,
+  strokeWidth = 1.5,
+  ...props
+}: IconProps) => (
+  <div className={clsx(`w-4 h-4`, className)} {...props}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
       <path fill="#f7fad1" d="M2 2h28v28H2z"></path>
       <path d="M20.809 23.875a2.866 2.866 0 0 0 2.6 1.6c1.09 0 1.787-.545 1.787-1.3c0-.9-.716-1.222-1.916-1.747l-.658-.282c-1.9-.809-3.16-1.822-3.16-3.964c0-1.973 1.5-3.476 3.853-3.476a3.889 3.889 0 0 1 3.742 2.107L25 18.128A1.789 1.789 0 0 0 23.311 17a1.145 1.145 0 0 0-1.259 1.128c0 .789.489 1.109 1.618 1.6l.658.282c2.236.959 3.5 1.936 3.5 4.133c0 2.369-1.861 3.667-4.36 3.667a5.055 5.055 0 0 1-4.795-2.691Zm-9.295.228c.413.733.789 1.353 1.693 1.353c.864 0 1.41-.338 1.41-1.653v-8.947h2.631v8.982c0 2.724-1.6 3.964-3.929 3.964a4.085 4.085 0 0 1-3.947-2.4Z"></path>
     </svg>
-  </IconWrapper>
-);
-
-const IconWrapper = ({
-  size = 24,
-  children,
-  className = "",
-  ...props
-}: PropsWithChildren<Omit<IconProps, "strokeWidth">>) => (
-  <div className={clsx(`w-${size / 4} h-${size / 4}`, className)} {...props}>
-    {children}
   </div>
 );
