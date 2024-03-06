f4f1d52cb7654bd3b8f59533d266b45b43fbfe2f
lhohw <lho.hw.an1@gmail.com>
2024-03-06 17:53:36 +0900

    feat: Use independently

--- app/posts/components/Posts.tsx
+++ app/posts/components/Posts.tsx
@@ -1,28 +1,30 @@
 import type { Post } from "@/app/lib/definitions";
-import { fetchLatestPosts } from "@/app/lib/actions";
+import Link from "next/link";
 
-export default async function Posts() {
-  const latestPosts = await fetchLatestPosts();
+export default async function Posts({ posts }: { posts: PostItemProps[] }) {
   return (
     <div className="w-full max-w-[808px] grid grid-cols-2 lg:grid-cols-4 p-2 gap-2 border-slight border-sea-200">
-      {latestPosts.map(({ title, src }) => (
-        <PostItem key={title} title={title} src={src} />
+      {posts.map((props) => (
+        <PostItem key={props.title} {...props} />
       ))}
     </div>
   );
 }
 
-function PostItem({ title, src }: Pick<Post, "title" | "src">) {
+export type PostItemProps = Pick<Post, "title" | "src"> & { href: string };
+function PostItem({ title, src, href }: PostItemProps) {
   return (
-    <div
-      className="relative flex min-w-20 min-h-20 justify-center text-center items-end pb-4 border-slight border-sea-200 cursor-pointer rounded-lg aspect-square"
-      style={{
-        background: `no-repeat bottom/100% url(${src}), #00000023`,
-        backgroundBlendMode: "darken",
-        backgroundSize: "cover",
-      }}
-    >
-      <span className="font-bold text-lg bottom-4 text-white">{title}</span>
-    </div>
+    <Link href={href}>
+      <div
+        className="relative flex min-w-20 min-h-20 justify-center text-center items-end pb-4 border-slight border-sea-200 cursor-pointer rounded-lg aspect-square"
+        style={{
+          background: `no-repeat bottom/100% url(${src}), #00000023`,
+          backgroundBlendMode: "darken",
+          backgroundSize: "cover",
+        }}
+      >
+        <span className="font-bold text-lg bottom-4 text-white">{title}</span>
+      </div>
+    </Link>
   );
 }
