245fbebcaf8d9c106fe41dffe4d1f8c0d114daaf
lhohw <lho.hw.an1@gmail.com>
2024-03-07 16:01:52 +0900

    fix: Remove unused style module

--- app/ui/posts/SideNavFallback.tsx
+++ app/ui/posts/SideNavFallback.tsx
@@ -1,12 +1,10 @@
-import styles from "@/app/ui/styles/util.module.css";
 import clsx from "clsx";
 
 export default async function SideNavFallback() {
   return (
     <div
       className={clsx(
-        "flex flex-col flex-1 h-full min-w-[295px] max-w-[393px] bg-darkgray rounded-tr-2xl border-slight border-sea-200 relative",
-        styles.fallback
+        "flex flex-col flex-1 h-full min-w-[295px] max-w-[393px] bg-darkgray rounded-tr-2xl border-slight border-sea-200 relative"
       )}
     ></div>
   );
