df20be0c14f9bf09734663f19860cf91cadde45e
lhohw <lho.hw.an1@gmail.com>
2024-03-11 14:43:21 +0900

    chore: prettier applied

--- app/posts/[category]/page.tsx
+++ app/posts/[category]/page.tsx
@@ -1,9 +1,12 @@
 import CategoryPosts from "@/app/ui/posts/[category]/CategoryPosts";
 import Post from "@/app/ui/posts/[category]/Post";
 
-export default function Page({ params, searchParams }: PageProps< { category: string }, { id?: string }>) {
+export default function Page({
+  params,
+  searchParams,
+}: PageProps<{ category: string }, { id?: string }>) {
   const { category } = params;
   const id = searchParams?.id;
   if (id == undefined) return <CategoryPosts category={category} />;
-  return <Post category={category} id={id} />
+  return <Post category={category} id={id} />;
 }
