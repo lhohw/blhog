aa7292ab8473e695f3e2010c81b18d4cd5be544f
lhohw <lho.hw.an1@gmail.com>
2024-03-30 22:48:52 +0900

    feat: Redirect to posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen pt-3 px-6 pb-6">
-      <div className="flex flex-col flex-1 bg-dark">
-        <Link
-          className="p-2 hover:text-sea-100 rounded-md border-slight border-sea-200"
-          href={"/posts"}
-        >
-          Posts
-        </Link>
-      </div>
-    </main>
-  );
+  redirect("/posts");
 }
