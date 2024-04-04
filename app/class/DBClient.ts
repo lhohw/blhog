df72a0395b542d0bfef81b49d5194edaef2b96a5
lhohw <lho.hw.an1@gmail.com>
2024-04-04 11:44:37 +0900

    feat: handle DB by env

--- app/class/DBClient.ts
+++ app/class/DBClient.ts
@@ -1,14 +1,31 @@
-// import { Pool } from "pg";
-import { VercelPoolClient, db } from "@vercel/postgres";
+import { Pool } from "pg";
+import { db } from "@vercel/postgres";
 
+type Client = {
+  connect: () => void;
+  query: <T = unknown>(
+    sql: { text: string; values: any[] } | string,
+  ) => Promise<{ rows: T[] }>;
+};
 class DBPool {
-  private static uniqueInstance: VercelPoolClient | null = null;
+  private static uniqueInstance: Client | null = null;
   private constructor() {}
   static async getInstance() {
     if (!DBPool.uniqueInstance) {
-      DBPool.uniqueInstance = await db.connect();
+      DBPool.uniqueInstance = await DBPool.connect();
     }
     return DBPool.uniqueInstance;
+  }
+  private static async connect() {
+    const env = process.env.NODE_ENV;
+    if (env === "production") {
+      const client = await db.connect();
+      return client;
+    } else {
+      const client = new Pool();
+      await client.connect();
+      return client;
+    }
   }
 }
 
