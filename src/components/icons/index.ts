5fa80456f3e9536433dade167f032561385b7b9f
lhohw <lho.hw.an1@gmail.com>
2024-05-30 21:10:03 +0900

    chore: add missing icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,11 +1,13 @@
 export type IconProps = { color?: string };
 
-export { default as JavaScript } from "./JavaScript";
-export { default as Menu } from "./Menu";
-export { default as React } from "./React";
-export { default as TypeScript } from "./TypeScript";
-export { default as ComputerScience } from "./ComputerScience";
-export { default as Circle } from "./Circle";
-export { default as Git } from "./Git";
-export { default as Mail } from "./Mail";
 export { default as Browser } from "./Browser";
+export { default as Circle } from "./Circle";
+export { default as ComputerScience } from "./ComputerScience";
+export { default as Git } from "./Git";
+export { default as JavaScript } from "./JavaScript";
+export { default as Mail } from "./Mail";
+export { default as Menu } from "./Menu";
+export { default as Moon } from "./Moon";
+export { default as React } from "./React";
+export { default as Sun } from "./Sun";
+export { default as TypeScript } from "./TypeScript";
