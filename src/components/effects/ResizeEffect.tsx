83a4a062b7d146f547a768fec8e5e01a4837ce2e
lhohw <lho.hw.an1@gmail.com>
2024-06-04 20:33:59 +0900

    chore: edit comment

--- src/components/effects/ResizeEffect.tsx
+++ src/components/effects/ResizeEffect.tsx
@@ -5,11 +5,10 @@ export type ResizeEffectProps = {
   isOpen: boolean;
   close: () => void;
 };
-/**
- * isOpen이나 close 변경 시 모두 항상 업데이트되기 때문에 deps 불필요
- *   -> useCallback 및 useEffect deps 제거 후 memo 적용
- * 새로운 기능 적용 시 해당 내용 변경 필요성 고려
- */
+
+// isOpen이나 close 변경 시 모두 항상 업데이트되기 때문에 deps 불필요
+//   -> useCallback 및 useEffect deps 제거 후 memo 적용
+// 새로운 기능 적용 시 해당 내용 변경 필요성 고려
 export default memo(function ResizeEffect({
   isOpen,
   close,
