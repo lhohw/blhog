194b8321acbe8d4300dc1344817b897973b0cb6f
lhohw <lho.hw.an1@gmail.com>
2024-06-12 15:52:29 +0900

    refactor: optimize re-rendering

--- src/components/organisms/PostSidebarImagesSection.tsx
+++ src/components/organisms/PostSidebarImagesSection.tsx
@@ -1,32 +1,41 @@
 "use client";
 
-import { useState } from "react";
+import { memo } from "react";
 import Image from "next/image";
 import Section from "@/components/molecules/Section";
 import Resizer from "@/components/atoms/Resizer";
-import PostImageEffect from "@/components/effects/PostImageEffect";
-import usePostImageContext from "@/hooks/react/usePostImageContext";
+import usePostImageContext, {
+  PostImageContext,
+} from "@/hooks/react/usePostImageContext";
 
 export default function PostSidebarImagesSection() {
-  const { images } = usePostImageContext();
-  const [max, setMax] = useState(100);
+  const { images, maxHeight } = usePostImageContext();
 
   return (
-    <>
-      <Resizer
-        className="hidden md:flex"
-        initialLength={100}
-        min={100}
-        max={max}
-        direction="top"
+    <Resizer
+      className="hidden md:flex"
+      initialLength={100}
+      min={100}
+      max={maxHeight}
+      direction="top"
+    >
+      <ImagesSection images={images} />
+    </Resizer>
+  );
+}
+
+// eslint-disable-next-line react/display-name
+const ImagesSection = memo(
+  ({ images }: { images: PostImageContext["images"] }) => {
+    return (
+      <Section
+        title="IMAGE"
+        titleClassName="text-xs md:text-xs pb-1"
+        className="h-full"
       >
-        <Section
-          title="IMAGE"
-          titleClassName="text-xs md:text-xs pb-1"
-          className="h-full"
-        >
-          <ul className="my-2 w-full h-full overflow-y-scroll snap-y snap-mandatory">
-            {images.map((img, idx) => (
+        <ul className="my-2 w-full h-full overflow-y-scroll snap-y snap-mandatory">
+          {images.map((img, idx) => {
+            return (
               <li key={idx} className="h-full w-full snap-center">
                 <Image
                   className="h-full w-full object-contain"
@@ -38,11 +47,10 @@ export default function PostSidebarImagesSection() {
                   title={img.title}
                 />
               </li>
-            ))}
-          </ul>
-        </Section>
-      </Resizer>
-      <PostImageEffect setMax={setMax} />
-    </>
-  );
-}
+            );
+          })}
+        </ul>
+      </Section>
+    );
+  },
+);
--- src/components/organisms/PostSidebarIndexSection.tsx
+++ src/components/organisms/PostSidebarIndexSection.tsx
@@ -1,46 +1,75 @@
 "use client";
 
+import { memo } from "react";
 import Link from "next/link";
 import { clsx } from "clsx";
 import usePostIndexContext from "@/hooks/react/usePostIndexContext";
 import Section from "@/components/molecules/Section";
-import PostIndexEffect from "@/components/effects/PostIndexEffect";
 
 export default function PostSidebarIndexSection() {
   const { postHeadings, currentIdx, onPostIndexClick } = usePostIndexContext();
 
   return (
-    <>
-      <Section
-        title="INDEX"
-        titleClassName="text-xs md:text-xs pb-1"
-        className="flex-1 mb-4 overflow-hidden min-h-24"
-      >
-        <ul className="h-full text-wrap overflow-y-scroll relative">
-          {postHeadings.map((heading, idx) => {
-            const { tagName, textContent, id } = heading;
-            const depth = parseInt(tagName.substring(1) || "0");
-
-            return (
-              <li
-                key={id}
-                className="text-text-alpha text-sm cursor-pointer transition-colors mt-1"
-                style={{ marginLeft: `${depth * 0.5}rem` }}
-              >
-                <Link
-                  className={clsx(idx === currentIdx && "text-primary")}
-                  data-post-heading-idx={idx}
-                  href={`#${id}`}
-                  onClick={onPostIndexClick}
-                >
-                  {textContent}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </Section>
-      <PostIndexEffect />
-    </>
+    <Section
+      title="INDEX"
+      titleClassName="text-xs md:text-xs pb-1"
+      className="flex-1 mb-4 overflow-hidden min-h-24"
+    >
+      <ul className="h-full text-wrap overflow-y-scroll relative">
+        {postHeadings.map((heading, idx) => {
+          const isCurrent = idx === currentIdx;
+          return (
+            <PostHeadingLi
+              key={heading.id}
+              isCurrent={isCurrent}
+              idx={idx}
+              onPostIndexClick={onPostIndexClick}
+              {...heading}
+            />
+          );
+        })}
+      </ul>
+    </Section>
   );
 }
+
+type PostHeadingLiProps = {
+  id: string;
+  tagName: string;
+  textContent: string;
+  isCurrent: boolean;
+  idx: number;
+  onPostIndexClick: React.MouseEventHandler;
+};
+// eslint-disable-next-line react/display-name
+const PostHeadingLi = memo(
+  ({
+    id,
+    tagName,
+    textContent,
+    isCurrent,
+    idx,
+    onPostIndexClick,
+  }: PostHeadingLiProps) => {
+    const depth = parseInt(tagName.substring(1) || "0");
+
+    return (
+      <li
+        className="text-text-alpha text-sm cursor-pointer transition-colors mt-1"
+        style={{ marginLeft: `${depth * 0.5}rem` }}
+      >
+        <Link
+          className={clsx(isCurrent && "text-primary")}
+          data-post-heading-idx={idx}
+          href={`#${id}`}
+          onClick={onPostIndexClick}
+        >
+          {textContent}
+        </Link>
+      </li>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.isCurrent === nextProps.isCurrent;
+  },
+);
