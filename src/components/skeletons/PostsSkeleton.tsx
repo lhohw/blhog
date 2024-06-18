f9b2d28be5bcd2a4b06db486314925725e73f64d
lhohw <lho.hw.an1@gmail.com>
2024-06-18 09:00:14 +0900

    refactor: split component

--- src/components/skeletons/PostsSkeleton.tsx
+++ src/components/skeletons/PostsSkeleton.tsx
@@ -1,7 +1,5 @@
-import ShimmerBar from "@/components/atoms/ShimmerBar";
-import CardWrapper from "@/components/atoms/wrapper/CardWrapper";
-import PhotoWrapper from "@/components/atoms/wrapper/PhotoWrapper";
 import Section from "@/components/molecules/Section";
+import CardSkeleton from "@/components/skeletons/CardSkeleton";
 
 export default function PostsSkeleton() {
   return (
@@ -14,19 +12,3 @@ export default function PostsSkeleton() {
     </Section>
   );
 }
-
-const CardSkeleton = () => (
-  <CardWrapper>
-    <div className="p-4 block border-slight border-primary rounded-lg">
-      <PhotoWrapper>
-        <ShimmerBar className="w-full h-full" />
-        <ShimmerBar className="w-3/4 h-3 mt-3 self-end" />
-      </PhotoWrapper>
-      <div className="mt-4 py-3">
-        <ShimmerBar className="w-3/4 h-4 mb-4" />
-        <ShimmerBar className="w-1/2 h-5 my-2" />
-        <ShimmerBar className="w-3/5 h-4 mt-4" />
-      </div>
-    </div>
-  </CardWrapper>
-);
