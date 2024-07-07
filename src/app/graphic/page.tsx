0688205157f65c27e10ad8d6696f991d4b9316d0
lhohw <lho.hw.an1@gmail.com>
2024-07-07 18:12:35 +0900

    feat: graphic page

---
+++ src/app/graphic/page.tsx
@@ -0,0 +1,13 @@
+import H3 from "@/components/atoms/headings/H3";
+import KineticTypography from "@/components/organisms/KineticTypography";
+
+export default function GraphicPage() {
+  return (
+    <>
+      <H3 className="mb-10" mainColor={false}>
+        Kinetic Typography
+      </H3>
+      <KineticTypography />
+    </>
+  );
+}
