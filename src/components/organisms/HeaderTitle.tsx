420bd527f9865add7b114ddbd08d8d9cf34362be
lhohw <lho.hw.an1@gmail.com>
2024-07-07 18:02:49 +0900

    feat: change logic to fit home

--- src/components/organisms/HeaderTitle.tsx
+++ src/components/organisms/HeaderTitle.tsx
@@ -9,29 +9,30 @@ import BackArrow from "@/components/icons/BackArrow";
 export default function HeaderTitle() {
   const router = useRouter();
   const pathname = usePathname();
-  const isPost = useMemo(() => pathname === "/post", [pathname]);
+  const isHome = useMemo(() => pathname === "/", [pathname]);
 
   const goBack = useCallback(() => {
-    if (!isPost) return;
+    if (isHome) return;
+
     router.back();
-  }, [router, isPost]);
+  }, [router, isHome]);
 
   return (
     <Link
       className="h-full flex items-center"
-      href="/posts"
+      href="/"
       aria-labelledby="blog-title"
     >
       <button
         className={clsx(
           "relative p-2 h-full transition-width duration-700",
-          isPost ? "w-16 cursor-pointer" : "w-0",
+          isHome ? "w-0" : "w-16 cursor-pointer",
         )}
         aria-label="go back"
-        tabIndex={isPost ? 0 : -1}
+        tabIndex={isHome ? 0 : -1}
         onClick={goBack}
       >
-        {isPost && (
+        {!isHome && (
           <span className="block absolute left-0 top-0 w-16 h-full">
             <BackArrow />
           </span>
