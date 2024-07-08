4a961a2465384e9e5fa9dee706ae388f45e947e0
lhohw <lho.hw.an1@gmail.com>
2024-07-08 16:15:00 +0900

    feat: change UI

--- src/app/graphic/kinetic-typography/page.tsx
+++ src/app/graphic/kinetic-typography/page.tsx
@@ -1,12 +1,18 @@
 import { Suspense } from "react";
 import KineticTypography from "@/components/organisms/KineticTypography";
+import H3 from "@/components/atoms/headings/H3";
 
 export default function KineticTypographyPage() {
   return (
-    <div className="flex flex-1">
-      <Suspense fallback="Loading kinetic typography">
-        <KineticTypography />
-      </Suspense>
+    <div className="flex flex-col text-center">
+      <H3 mainColor={false} className="p-4">
+        Kinetic Typography
+      </H3>
+      <div className="flex flex-1 mt-6">
+        <Suspense fallback="Loading kinetic typography">
+          <KineticTypography />
+        </Suspense>
+      </div>
     </div>
   );
 }
