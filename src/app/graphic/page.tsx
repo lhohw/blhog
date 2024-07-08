87d6b45e28cb4d07f422ac86efe59b399f907a98
lhohw <lho.hw.an1@gmail.com>
2024-07-08 23:29:40 +0900

    feat: temporal graphic page

--- src/app/graphic/page.tsx
+++ src/app/graphic/page.tsx
@@ -1,13 +1,26 @@
-import H3 from "@/components/atoms/headings/H3";
-import KineticTypography from "@/components/organisms/KineticTypography";
+import Link from "next/link";
+import { strToSlug, toPascalCase } from "@/lib/utils/string";
 
 export default function GraphicPage() {
   return (
-    <>
-      <H3 className="mb-10" mainColor={false}>
-        Kinetic Typography
-      </H3>
-      <KineticTypography />
-    </>
+    <div className="flex flex-col">
+      <LinkBox title="Custom Checkbox" />
+      <LinkBox title="Animated Back" />
+      <LinkBox title="Kinetic Typography" />
+    </div>
   );
 }
+
+const LinkBox = ({ title }: { title: string }) => (
+  <Link
+    className={`
+      relative area py-2 px-4 rounded-lg [&+&]:mt-4 text-center overflow-hidden transition-colors duration-300
+      before:-z-10 before:absolute before:inset-0 before:bg-primary-alpha before:-translate-x-full
+      before:transition-transform before:duration-300
+      hover:text-white hover:before:-translate-x-0 
+    `}
+    href={`/graphic/${strToSlug(title).toLocaleLowerCase()}`}
+  >
+    {toPascalCase(title)}
+  </Link>
+);
