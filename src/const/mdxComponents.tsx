b07552b4e630129bc82342404c743af47625e853
lhohw <lho.hw.an1@gmail.com>
2024-07-12 23:02:01 +0900

    feat: handle filename(.md) in server

--- src/const/mdxComponents.tsx
+++ src/const/mdxComponents.tsx
@@ -1,21 +1,16 @@
 import type { MDXRemoteProps } from "next-mdx-remote/rsc";
 import clsx from "clsx";
 import { createMDXHeadings } from "@/lib/utils/markdown";
-import { httpRegex, mdLinkRegex } from "@/const/regex";
+import { httpRegex } from "@/const/regex";
 
 const components: MDXRemoteProps["components"] = {
   a: ({ children, className, href = "", title, ...props }) => {
     const isURI = children === href;
     href = decodeURIComponent(href);
     const isHttp = href.match(httpRegex);
-    if (!isHttp) {
-      const matched = mdLinkRegex.exec(href);
-      if (matched) {
-        href = href.replace(matched[0], matched[1] || "");
-      }
-    } else if (isURI) {
-      children = href;
-    }
+
+    if (isURI) children = href;
+
     return (
       <a
         className={clsx(className, "main-color")}
--- src/const/regex.ts
+++ src/const/regex.ts
@@ -1,3 +1,1 @@
-export const mdLinkRegex = /(?:.md)(#?\/?[-\w\d가-힣ㄱ-ㅎㅏ-ㅣ]+)?$/;
-
 export const httpRegex = /^http[s]?:\/\//;
