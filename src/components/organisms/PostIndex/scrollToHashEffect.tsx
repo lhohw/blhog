131a1a6440c1bfdbd7927d2763d7709c5d920dd0
lhohw <lho.hw.an1@gmail.com>
2024-07-12 23:03:15 +0900

    fix: invalid querySelector access by hash

--- src/components/organisms/PostIndex/scrollToHashEffect.tsx
+++ src/components/organisms/PostIndex/scrollToHashEffect.tsx
@@ -2,15 +2,23 @@
 
 import { useEffect } from "react";
 import { FOLDED_SIDEBAR_SIZE_UNDER_MD } from "@/const/size";
+import { getAllHeadingsInPost } from "@/lib/utils/dom";
 
 export default function ScrollToHashEffect() {
   useEffect(() => {
-    const { hash } = location;
+    let { hash } = location;
     if (!hash) return;
 
-    const target = document.querySelectorAll(hash)?.[1] as HTMLHeadingElement;
+    hash = hash.substring(1);
+    hash = decodeURIComponent(hash);
+
+    const target = getAllHeadingsInPost().find((heading) =>
+      heading.id.match(hash),
+    );
+
     if (target) {
       const { offsetTop } = target;
+
       window.scrollTo({
         top: offsetTop - FOLDED_SIDEBAR_SIZE_UNDER_MD,
         behavior: "smooth",
