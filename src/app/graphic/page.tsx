1aa52f1ddf1de224e889d80878713ee2de7a4c5b
lhohw <lho.hw.an1@gmail.com>
2024-07-17 18:39:11 +0900

    feat: add easing route

--- src/app/graphic/page.tsx
+++ src/app/graphic/page.tsx
@@ -5,6 +5,7 @@ export default function GraphicPage() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <LinkBox title="Kinetic Typography" />
+      <LinkBox title="Easing" />
     </div>
   );
 }
